30be0a025c8c026e5f087a51309ff609e5d02e84
sarah-shtawi <sarah.sht03@gmail.com>
2024-10-05 23:14:50 +0300

    Dashored 1

---
+++ MVCProject1.PL/Areas/Dashboard/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject1.PL.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
